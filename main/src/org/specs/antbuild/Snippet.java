91bff24f7d47f618707cbf02dc6dfc6b457378bb
joaobispo <joaobispo@gmail.com>
2015-01-04 18:09:34 +0000

    - Updated projectSet.psf to include a Gradle project;

--- main/src/org/specs/antbuild/Snippet.java
+++ main/src/org/specs/antbuild/Snippet.java
@@ -7,7 +7,7 @@ public class Snippet {
 
 	public static void main(String[] args) {
 		System.out.println("DUMMY:"+new Dummy());
-		System.out.println("DUMMY TEST:"+new DummyTest());
+		new DummyTest().test();
 
 	}
 
