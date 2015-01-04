f1caca7bafea8a1d01c4a8a9c890bc31013761c2
joaobispo <joaobispo@gmail.com>
2015-01-04 17:39:31 +0000

    - Added dependency to specs-library-tests;

---
+++ main/src/org/specs/antbuild/Snippet.java
@@ -0,0 +1,14 @@
+package org.specs.antbuild;
+
+import org.specs.library.Dummy;
+import org.specs.library.DummyTest;
+
+public class Snippet {
+
+	public static void main(String[] args) {
+		System.out.println("DUMMY:"+new Dummy());
+		System.out.println("DUMMY TEST:"+new DummyTest());
+
+	}
+
+}
