3df52f5bf5236a17ca200fa72fe9de66837b7c3b
joaobispo <joaobispo@gmail.com>
2015-01-05 00:53:17 +0000

    - Added dependency to specs-library;

--- ant-build-for-java-eclipse/src/main/java/org/specs/antbuilder/Person.java
+++ ant-build-for-java-eclipse/src/main/java/org/specs/antbuilder/Person.java
@@ -1,22 +1,20 @@
 package org.specs.antbuilder;
 
-import org.gradle.Person2;
-
 
 public class Person {
-    private final String name;
+	private final String name;
 
-    public Person(String name) {
-        this.name = name;
-        
-        System.out.println(new Person2().getVersion());
-    }
+	public Person(String name) {
+		this.name = name;
 
-    public String getName() {
-        return name;
-    }
-    
-    public static void main(String[] args) {
-    	new Person("joao");
-    }
+		// System.out.println(new Person2().getVersion());
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public static void main(String[] args) {
+		new Person("joao");
+	}
 }
